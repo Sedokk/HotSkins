7a224b1fe5c7bc5c4a23a4f997bda41750b2b013
Gleb <st090625@gmail.com>
2023-03-01 22:23:04 +0300

    started HotSkins developing

---
+++ src/App/App.jsx
@@ -0,0 +1,10 @@
+import style from "./App.module.scss"
+function App() {
+  return (
+    <div className='App'>
+      <h1 className={style.label}>Start developing</h1>
+    </div>
+  )
+}
+
+export default App
