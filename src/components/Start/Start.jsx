47b2f26eec9eb5f2505bf2febd4d5682a1544dde
Gleb <st090625@gmail.com>
2023-03-03 16:40:24 +0300

    added content to start page

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -2,7 +2,16 @@ import React from "react"
 import style from "./Start.module.scss"
 
 const Start = () => {
-  return <div className={style.wrapper}></div>
+  return (
+    <div className={style.wrapper}>
+      <div className={style.contentWrapper}>
+        <h1 className={style.bigSign}>
+          Принимайте платежи от пользователей с помощью скинов CS:GO и DOTA 2
+        </h1>
+        <button className={style.btn}>Присоединиться</button>
+      </div>
+    </div>
+  )
 }
 
 export default Start
