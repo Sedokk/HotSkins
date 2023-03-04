171f6bef64369ffad8463ed5484c6cbb47fcb012
Gleb <st090625@gmail.com>
2023-03-04 20:51:51 +0300

    added registartion field

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import style from "./Header.module.scss"
 
 const Header = () => {
   return (
     <header className={style.header}>
-      <div className={style.logoWrapper}>
-        <img src='./img/logo/logo.svg' alt='logo' />
-        <img src='./img/logo/logoText.svg' alt='label' />
-      </div>
+      <Link to='/'>
+        <div className={style.logoWrapper}>
+          <img src='./img/logo/logo.svg' alt='logo' />
+          <img src='./img/logo/logoText.svg' alt='label' />
+        </div>
+      </Link>
       <nav className={style.nav}>
         <ul className={style.navList}>
           <li className={style.navItem}>Помощь</li>
---
+++ src/components/Registration/Registration.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import style from "./Registration.module.scss"
+
+const Registration = () => {
+  return (
+    <div className={style.wrapper}>
+      <img src='./img/connectionLogo.png' alt='logo' className={style.img} />
+      <form className={style.form}>
+        <h2 className={style.title}>Заявка на подключение</h2>
+        <label className={style.label}>
+          <input type='text' className={style.input} placeholder='Ваше имя' />
+        </label>
+        <label className={style.label}>
+          <input type='text' className={style.input} placeholder='Ваш email' />
+        </label>
+        <label className={style.label}>
+          <input
+            type='text'
+            className={style.input}
+            placeholder='Адрес сайта'
+          />
+        </label>
+        <label className={style.label}>
+          <textarea
+            type='text'
+            className={`${style.input} ${style.textArea}`}
+            placeholder='Ваши контакты (Телефон, Telegram, WhatsApp...)'
+            rows={4}
+          />
+        </label>
+        <button className={style.formBtn}>Зарегестрироваться</button>
+      </form>
+    </div>
+  )
+}
+
+export default Registration
