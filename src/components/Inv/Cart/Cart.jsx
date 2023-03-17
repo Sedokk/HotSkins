965de89797b86d3693c424e7d87fbe75b1f77c49
Gleb <st090625@gmail.com>
2023-03-17 13:46:19 +0300

    added some style changes

--- src/components/Inv/Cart/Cart.jsx
+++ src/components/Inv/Cart/Cart.jsx
@@ -1,16 +1,29 @@
 import React from "react"
+import { useCart } from "../../../store"
 import style from "./Cart.module.scss"
 import CartLayout from "./CartLayout/CartLayout"
 import Results from "./Results/Results"
 
 const Cart = () => {
+  const cart = useCart((state) => state.cart)
   return (
     <section className={`container ${style.cart}`}>
       <div className={style.itemsContainer}>
         <h2 className={style.cartTitle}>Обмен преметов</h2>
-        <div className={style.itemsWrapper}>
-          <CartLayout />
-        </div>
+        {cart.length > 0 ? (
+          <div className={style.itemsWrapper}>
+            <CartLayout />
+          </div>
+        ) : (
+          <div className={style.notFound}>
+            <img
+              src='./img/other/eye.png'
+              alt='no items'
+              style={{ width: "300px" }}
+            />
+            <p>Ваша корзина пуста</p>
+          </div>
+        )}
       </div>
       <Results />
     </section>
